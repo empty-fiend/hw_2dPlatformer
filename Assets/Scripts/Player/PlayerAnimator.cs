bee63a745db05cab2069560693ca4b7278bf30fb
empty_fiend <empty.fiend@icloud.com>
2021-08-17 00:12:17 +0300

    Code corrections
    Исправлена ошибка в компонентах PlayerAnimator и PlayerDircetionChanger

--- Assets/Scripts/Player/PlayerAnimator.cs
+++ Assets/Scripts/Player/PlayerAnimator.cs
@@ -1,17 +1,20 @@
 using UnityEngine;
 
 [RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(PlayerButtons))]
 public class PlayerAnimator : MonoBehaviour
 {
     private Animator _animator;
+    private PlayerButtons _playerButtons;
 
     private void Start()
     {
         _animator = GetComponent<Animator>();
+        _playerButtons = GetComponent<PlayerButtons>();
     }
 
     private void Update()
     {
-        _animator.SetBool("IsWalking", Input.GetAxis("Horizontal") != 0);
+        _animator.SetBool("IsWalking", Input.GetKey(_playerButtons.Right) || Input.GetKey(_playerButtons.Left));
     }
 }
--- Assets/Scripts/Player/PlayerDirectionChanger.cs
+++ Assets/Scripts/Player/PlayerDirectionChanger.cs
@@ -1,18 +1,21 @@
 using UnityEngine;
 
+[RequireComponent(typeof(PlayerButtons))]
 public class PlayerDirectionChanger : MonoBehaviour
 {
     private float _newPlayerScaleX;
+    private PlayerButtons _playerButtons;
 
     private void Start()
     {
+        _playerButtons = GetComponent<PlayerButtons>();
         _newPlayerScaleX = transform.localScale.x;
     }
     private void Update()
     {
-        if (Input.GetAxis("Horizontal") > 0)
+        if (Input.GetKeyDown(_playerButtons.Right))
             _newPlayerScaleX = 1;
-        else if (Input.GetAxis("Horizontal") < 0)
+        else if (Input.GetKeyDown(_playerButtons.Left))
             _newPlayerScaleX = -1;
 
         transform.localScale = new Vector3(_newPlayerScaleX, 1, 1);
